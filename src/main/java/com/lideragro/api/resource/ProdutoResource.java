cedb2bbd952127934f8c4a02ec299195a1a3c484
Fellipe <fellipepf@gmail.com>
2018-10-09 13:57:13 -0300

    enable cross origin requests by filter (cors)

--- src/main/java/com/lideragro/api/resource/ProdutoResource.java
+++ src/main/java/com/lideragro/api/resource/ProdutoResource.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import com.lideragro.api.model.Produto;
 import com.lideragro.api.repository.ProdutoRepository;
 import com.lideragro.api.service.ProdutoService;
 
+//@CrossOrigin(maxAge = 10)
 @RestController
 @RequestMapping("/produto")
 public class ProdutoResource {
