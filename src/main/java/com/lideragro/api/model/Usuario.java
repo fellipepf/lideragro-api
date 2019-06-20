62e94ae9545603ccd738259fa957747d4054a794
fellipe <fellipepf@gmail.com>
2019-06-20 08:52:41 +0100

    creating new user
    This reverts commit 45c6cc8120aca91bae31e998b98b6ca10352bd6e.

--- src/main/java/com/lideragro/api/model/Usuario.java
+++ src/main/java/com/lideragro/api/model/Usuario.java
@@ -2,8 +2,11 @@ package com.lideragro.api.model;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -15,6 +18,7 @@ import javax.persistence.Table;
 public class Usuario {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
 	private String nome;
--- src/main/java/com/lideragro/api/security/util/GeradorSenha.java
+++ src/main/java/com/lideragro/api/security/util/GeradorSenha.java
@@ -4,6 +4,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class GeradorSenha {
 	
+	public static String passwordEncode(String rawPassword) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		return encoder.encode(rawPassword);
+	}
+	
 	public static void main(String[] args) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		System.out.println(encoder.encode("admin"));
