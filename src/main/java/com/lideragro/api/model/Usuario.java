45c6cc8120aca91bae31e998b98b6ca10352bd6e
fellipe <fellipepf@gmail.com>
2019-06-20 08:52:10 +0100

    Revert "password encoder"
    This reverts commit b82949c9ba1f4b6877818eaccdfd74b30c7198c0.

--- src/main/java/com/lideragro/api/model/Usuario.java
+++ src/main/java/com/lideragro/api/model/Usuario.java
@@ -2,11 +2,8 @@ package com.lideragro.api.model;
 
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -18,7 +15,6 @@ import javax.persistence.Table;
 public class Usuario {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
 	private String nome;
--- src/main/java/com/lideragro/api/security/util/GeradorSenha.java
+++ src/main/java/com/lideragro/api/security/util/GeradorSenha.java
@@ -4,12 +4,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class GeradorSenha {
 	
-	public static String passwordEncode(String rawPassword) {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
-		return encoder.encode(rawPassword);
-	}
-	
 	public static void main(String[] args) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		System.out.println(encoder.encode("admin"));
